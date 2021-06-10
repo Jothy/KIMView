909034d10ab0df152bf6d0b8a7ce0fd9af769d75
Jothy Selvaraj <Jothybasu@gmail.com>
2021-06-10 12:33:46 +1000

    Minor

--- bevwidget.cpp
+++ bevwidget.cpp
@@ -64,7 +64,6 @@ BEVWidget::BEVWidget(QWidget *parent, QActionGroup *contextMenus)
   //    this->ModelRenderer2->SetLayer(1);
   //    this->ui->BEVWindow->GetRenderWindow()->SetNumberOfLayers(2);
 
-
   this->ImageStyleImg = vtkSmartPointer<vtkInteractorStyleImage>::New();
   this->Interactor3D = vtkSmartPointer<vtkRenderWindowInteractor>::New();
   this->InteractorTrackball =
@@ -195,32 +194,29 @@ void BEVWidget::DisplayBEV() {
 }
 
 void BEVWidget::DisplayMeshes() {
-    // qDebug()<<"No. of meshes found:"<<this->MeshActors->GetNumberOfItems();
-    this->MeshActors->InitTraversal();
-    for (vtkIdType i = 0; i < this->MeshActors->GetNumberOfItems(); i++) {
-        if(this->ROITypes[i]=="EXTERNAL"){
-            vtkActor *CurActor = this->MeshActors->GetNextItem();
-            CurActor->GetProperty()->SetColor(0.8,0.8,0.8);
-            CurActor->GetProperty()->SetOpacity(0.75);
-            this->ModelRenderer->AddViewProp(CurActor);
-        }
-        else{
-            double R = this->ROIColors[i][0] / 255.0;
-            double G = this->ROIColors[i][1] / 255.0;
-            double B = this->ROIColors[i][2] / 255.0;
-            vtkActor *CurActor = this->MeshActors->GetNextItem();
-            CurActor->GetProperty()->SetColor(R, G, B);
-            this->ModelRenderer->AddViewProp(CurActor);
-
-        }
-
+  // qDebug()<<"No. of meshes found:"<<this->MeshActors->GetNumberOfItems();
+  this->MeshActors->InitTraversal();
+  for (vtkIdType i = 0; i < this->MeshActors->GetNumberOfItems(); i++) {
+    if (this->ROITypes[i] == "EXTERNAL") {
+      vtkActor *CurActor = this->MeshActors->GetNextItem();
+      CurActor->GetProperty()->SetColor(0.8, 0.8, 0.8);
+      CurActor->GetProperty()->SetOpacity(0.75);
+      this->ModelRenderer->AddViewProp(CurActor);
+    } else {
+      double R = this->ROIColors[i][0] / 255.0;
+      double G = this->ROIColors[i][1] / 255.0;
+      double B = this->ROIColors[i][2] / 255.0;
+      vtkActor *CurActor = this->MeshActors->GetNextItem();
+      CurActor->GetProperty()->SetColor(R, G, B);
+      this->ModelRenderer->AddViewProp(CurActor);
     }
+  }
 
-    this->ModelRenderer->GetActiveCamera()->SetViewUp(0, -1, 0);
-    this->ModelRenderer->GetActiveCamera()->SetFocalPoint(0.0, 0.0, 0.0);
-    this->ModelRenderer->GetActiveCamera()->SetPosition(0.0, 0.0, -1.0);
-    this->ModelRenderer->GetRenderWindow()->Render();
-    this->ModelRenderer->GetRenderWindow()->GetInteractor()->Start();
+  this->ModelRenderer->GetActiveCamera()->SetViewUp(0, -1, 0);
+  this->ModelRenderer->GetActiveCamera()->SetFocalPoint(0.0, 0.0, 0.0);
+  this->ModelRenderer->GetActiveCamera()->SetPosition(0.0, 0.0, -1.0);
+  this->ModelRenderer->GetRenderWindow()->Render();
+  this->ModelRenderer->GetRenderWindow()->GetInteractor()->Start();
 }
 
 void BEVWidget::DisplayBeams() {
--- main.cpp
+++ main.cpp
@@ -37,11 +37,11 @@ int main(int argc, char *argv[]) {
   a.setOrganizationDomain("https://image-x.sydney.edu.au/");
   a.setApplicationName("KIMView");
 
-  //Default style
-  QFile file(":/Styles/ElegantDark.qss");
-  file.open(QFile::ReadOnly);
-  QString styleSheet = QLatin1String(file.readAll());
-  a.setStyleSheet(styleSheet);
+  //  // Default style
+  //  QFile file(":/Styles/ElegantDark.qss");
+  //  file.open(QFile::ReadOnly);
+  //  QString styleSheet = QLatin1String(file.readAll());
+  //  a.setStyleSheet(styleSheet);
 
   return a.exec();
 }
