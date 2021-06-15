8ea8b37e10227989807f7e8fec74fc5a988d39a5
Jothy Selvaraj <Jothybasu@gmail.com>
2021-06-15 16:23:20 +1000

    VTK charting enabled and Chart item added to testing menu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -122,6 +122,20 @@ SOFTWARE.
 #include <vtkPolarAxesActor.h>
 #include <vtkTransform.h>
 
+// Charts test
+#include <vtkChartLegend.h>
+#include <vtkChartXY.h>
+#include <vtkContextScene.h>
+#include <vtkContextView.h>
+#include <vtkFloatArray.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPlotPoints.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkTable.h>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
@@ -979,8 +993,75 @@ void MainWindow::on_actionMove_ROI_triggered() {
 }
 
 void MainWindow::on_actionRotate_ROI_triggered() {
-  // qDebug() << this->AxialViewer->ImageSlice->GetInput()->GetScalarRange()[0];
-  // qDebug() << this->AxialViewer->ImageSlice->GetInput()->GetScalarRange()[1];
+  vtkNew<vtkNamedColors> colors;
+
+  // Set up a 2D scene, add an XY chart to it
+  vtkSmartPointer<vtkContextView> view = vtkSmartPointer<vtkContextView>::New();
+  view->GetRenderWindow()->SetSize(400, 300);
+  view->GetRenderWindow()->SetWindowName("ScatterPlot");
+  view->GetRenderer()->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+  vtkSmartPointer<vtkChartXY> chart = vtkSmartPointer<vtkChartXY>::New();
+  view->GetScene()->AddItem(chart);
+  chart->SetShowLegend(true);
+  chart->GetLegend()->SetDragEnabled(true);
+  chart->GetLegend()->SetInteractive(true);
+  // view->GetRenderer()->UseFXAAOn();//no effect
+
+  // Create a table with some points in it...
+  vtkSmartPointer<vtkTable> table = vtkSmartPointer<vtkTable>::New();
+
+  vtkSmartPointer<vtkFloatArray> arrX = vtkSmartPointer<vtkFloatArray>::New();
+  arrX->SetName("X Axis");
+  table->AddColumn(arrX);
+
+  vtkSmartPointer<vtkFloatArray> arrC = vtkSmartPointer<vtkFloatArray>::New();
+  arrC->SetName("Cosine");
+  table->AddColumn(arrC);
+
+  vtkSmartPointer<vtkFloatArray> arrS = vtkSmartPointer<vtkFloatArray>::New();
+  arrS->SetName("Sine");
+  table->AddColumn(arrS);
+
+  vtkSmartPointer<vtkFloatArray> arrT = vtkSmartPointer<vtkFloatArray>::New();
+  arrT->SetName("Sine - Cosine");
+  table->AddColumn(arrT);
+
+  // Test charting with a few more points...
+  int numPoints = 40;
+  float inc = 7.5 / (numPoints - 1);
+  table->SetNumberOfRows(numPoints);
+  for (int i = 0; i < numPoints; ++i) {
+    table->SetValue(i, 0, i * inc);
+    table->SetValue(i, 1, cos(i * inc) + 0.0);
+    table->SetValue(i, 2, sin(i * inc) + 0.0);
+    table->SetValue(i, 3, sin(i * inc) - cos(i * inc));
+  }
+
+  // Add multiple scatter plots, setting the colors etc
+  vtkPlot *points = chart->AddPlot(vtkChart::LINE);
+  points->SetInputData(table, 0, 1);
+  points->SetColor(255, 0, 0, 255);
+  points->SetWidth(1.0);
+  dynamic_cast<vtkPlotPoints *>(points)->SetMarkerStyle(vtkPlotPoints::CROSS);
+
+  points = chart->AddPlot(vtkChart::LINE);
+  points->SetInputData(table, 0, 2);
+  points->SetColor(0, 255, 0, 255);
+  points->SetWidth(1.0);
+  dynamic_cast<vtkPlotPoints *>(points)->SetMarkerStyle(vtkPlotPoints::PLUS);
+
+  points = chart->AddPlot(vtkChart::LINE);
+  points->SetInputData(table, 0, 3);
+  points->SetColor(0, 0, 255, 255);
+  points->SetWidth(1.0);
+  dynamic_cast<vtkPlotPoints *>(points)->SetMarkerStyle(vtkPlotPoints::CIRCLE);
+
+  // Finally render the scene
+  view->GetRenderWindow()->SetMultiSamples(5);  // Enable anti-aliasing
+  view->GetRenderWindow()->Render();
+  view->GetInteractor()->Initialize();
+  view->GetInteractor()->Start();
 }
 
 void MainWindow::on_actionAdd_Arc_triggered() {
